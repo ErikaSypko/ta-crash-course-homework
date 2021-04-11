2ec75f97c8b5d6a6027423d9854af850cf895e4b
Erika Sypko <sypko.erika.2000@gmail.com>
2021-04-11 05:25:45 +0300

    added task 2a

--- HW2/src/com/company/Main.java
+++ HW2/src/com/company/Main.java
@@ -4,36 +4,53 @@ import java.util.Scanner;
 
 public class Main {
     static void firstTask(double a, double b, double c, double x, double y) {
-
+        System.out.println("Will the brick pass through?");
         if (a < 0 || b < 0 || c < 0 || x < 0 || y < 0) {
-            System.out.println("Число має бути невід'ємним!");
+            System.out.println("The number must be non-negative!");
             return;
         }
         if ((a < x && b < y || a < y && b < x)
-                || (a < x && c < y || a< y && c < x)
+                || (a < x && c < y || a < y && c < x)
                 || (b < x && c < y || b < y && c < x)) {
-            System.out.println("Так.");
+            System.out.println("Yes.");
+        } else {
+            System.out.println("No.");
         }
-        else {
-            System.out.println("Ні.");
+    }
+    static void secondTaskA(int n){
+        String strN = String.valueOf(n*n);
+        if(strN.contains("3")){
+            System.out.println("Number contains 3");
         }
+        else{
+            System.out.printf("Number %d does not contains 3",Integer.parseInt(strN));
+        }
+
     }
 
     public static void main(String[] args) {
-        System.out.println("Завдання 1:");
+        System.out.println("Task 1:");
         Scanner in = new Scanner(System.in);
-        double a ,b,c,x,y;
-        System.out.print("Введіть a: ");
+        double a, b, c, x, y;
+        System.out.print("Input a: ");
         a = in.nextDouble();
-        System.out.print("Введіть b: ");
+        System.out.print("Input b: ");
         b = in.nextDouble();
-        System.out.print("Введіть c: ");
+        System.out.print("Input c: ");
         c = in.nextDouble();
-        System.out.print("Введіть x: ");
+        System.out.print("Input x: ");
         x = in.nextDouble();
-        System.out.print("Введіть y: ");
+        System.out.print("Input y: ");
         y = in.nextDouble();
-        firstTask(a,b,c,x,y);
+        firstTask(a, b, c, x, y);
         System.out.println();
+
+        System.out.println("Task 2:");
+        int n;
+        System.out.print("Input n: ");
+        n = in.nextInt();
+        secondTaskA(n);
+
+
     }
 }
