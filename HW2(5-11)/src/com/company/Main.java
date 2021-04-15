e304c4b4e647d217d27dbd05c1807909f0ffc0cd
Sypko Erika <sypko.erika.2000@gmail.com>
2021-04-15 17:42:48 +0300

    solved 9 task

--- HW2(5-11)/src/com/company/Main.java
+++ HW2(5-11)/src/com/company/Main.java
@@ -4,13 +4,43 @@ import eighthTask.ContractEmployee;
 import eighthTask.Employee;
 import eighthTask.SalariedEmployee;
 import seventhTask.*;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
+
+    public static double Div(double a, double b) throws Exception {
+        if (b == 0) throw new Exception("Сannot be divided by zero!");
+        return a / b;
+    }
+
+    public static double readNumber(double start, double end) throws Exception {
+        System.out.printf("Input number in range (%d,%d): ", (int) start, (int) end);
+        System.out.println();
+        Scanner in = new Scanner(System.in);
+        Double inputNum;
+        try {
+            inputNum = in.nextDouble();
+        } catch (InputMismatchException e) {
+            throw new Exception("Non-number text");
+        }
+        if (inputNum <= start || inputNum >= end)
+            throw new Exception("Invalid range");
+
+
+        return inputNum;
+    }
+
+    public static void Task9(int n, double start, double end) throws Exception {
+        double temp = readNumber(start, end);
+        int count = 1;
+        while (n > 1) {
+            System.out.println(count + ") " + temp);
+            temp = readNumber(temp, end);
+            n = n - 1;
+            count++;
+        }
+        System.out.println(count + ") " + temp);
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Task 7:");
@@ -34,15 +64,23 @@ public class Main {
             employeeList.add(new ContractEmployee("SE883", "Sophia", 2899));
             employeeList.add(new ContractEmployee("QC854", "John", 5000));
             employeeList.add(new ContractEmployee("AR156", "Joanne", 3800));
-            employeeList.add(new SalariedEmployee("456-22-1920", "Angelina", 15,7));
+            employeeList.add(new SalariedEmployee("456-22-1920", "Angelina", 15, 7));
             employeeList.add(new ContractEmployee("SE883", "Martha", 1899));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         Collections.sort(employeeList, Collections.reverseOrder());
         for (var e : employeeList) {
             System.out.println(e.toString());
         }
+
+        System.out.println("\nTask 9:");
+        try {
+            System.out.println("div: " + Div(12.5, 3));
+            Task9(10, 1, 100);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
